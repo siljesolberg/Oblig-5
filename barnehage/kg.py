6603652f5666bc5d2a18167319f3fc7882199a66
sivertdorum <sivertd@uia.no>
2024-11-19 11:13:32 +0100

    remote push

--- barnehage/kg.py
+++ barnehage/kg.py
@@ -4,23 +4,25 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import session
-from kgmodel import (Foresatt, Barn, Soknad, Barnehage)
 from kgcontroller import (form_to_object_soknad,
                           insert_soknad, commit_all,
                           select_alle_barnehager,
-                          select_alle_soknader,get_all_data)
+                          select_alle_soknader, get_all_data)
 
 app = Flask(__name__)
-app.secret_key = 'BAD_SECRET_KEY' # nødvendig for session
+app.secret_key = 'BAD_SECRET_KEY'  # nødvendig for session
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/barnehager')
 def barnehager():
     information = select_alle_barnehager()
     return render_template('barnehager.html', data=information)
+
 
 @app.route('/behandle', methods=['GET', 'POST'])
 def behandle():
@@ -30,14 +32,16 @@ def behandle():
         log = insert_soknad(form_to_object_soknad(sd))
         print(log)
         session['information'] = sd
-        return redirect(url_for('svar')) #[1]
+        return redirect(url_for('svar'))  # [1]
     else:
         return render_template('soknad.html')
+
 
 @app.route('/svar')
 def svar():
     information = session['information']
     return render_template('svar.html', data=information)
+
 
 @app.route('/commit')
 def commit():
@@ -45,24 +49,15 @@ def commit():
     information = get_all_data()
     return render_template('commit.html', data=information)
 
+
 @app.route('/soknader')
 def soknader():
-    information = select_alle_soknader()
-    return render_template('soknader.html', soknader=information)
+    real_data = select_alle_soknader()
+    print("Debug: Current state of soknader data:", real_data)
+    return render_template('soknader.html', soknader=real_data)
+
 
 @app.route('/statistikk')
 def statistikk():
+
     return render_template('statistikk.html')
-
-
-
-
-"""
-Referanser
-[1] https://stackoverflow.com/questions/21668481/difference-between-render-template-and-redirect
-"""
-
-"""
-Søkeuttrykk
-
-"""
--- barnehage/kgmodel.py
+++ barnehage/kgmodel.py
@@ -1,14 +1,5 @@
-# kgmodel module
-
-# For å unngå å skrive konstruktører (se [1])
 from dataclasses import dataclass
 
-# ----- Definerer modellen ------
-"""
-Empty DataFrame
-Columns: [foresatt_id, foresatt_navn, foresatt_adresse, foresatt_tlfnr, foresatt_pnr]
-Index: []
-"""
 @dataclass
 class Foresatt:
     foresatt_id: int
@@ -17,22 +8,11 @@ class Foresatt:
     foresatt_tlfnr: str
     foresatt_pnr: str
 
-
-"""
-Empty DataFrame
-Columns: [barn_id, barn_pnr]
-Index: []
-"""
 @dataclass
 class Barn:
     barn_id : int
     barn_pnr : str
     
-"""
-Index(['barnehage_id', 'barnehage_navn', 'barnehage_antall_plasser',
-       'barnehage_ledige_plasser'],
-      dtype='object')
-"""
 @dataclass
 class Barnehage:
     barnehage_id: int
@@ -40,13 +20,6 @@ class Barnehage:
     barnehage_antall_plasser: int
     barnehage_ledige_plasser: int
     
-"""
-Empty DataFrame
-Columns: [foresatt_1, foresatt_2, barn_1, fr_barnevern, fr_sykd_familie,
-fr_sykd_barn, fr_annet, barnehager_prioritert, sosken__i_barnehagen,
-tidspunkt_oppstart, brutto_inntekt]
-Index: []
-"""
 @dataclass
 class Soknad:
     sok_id: int
@@ -61,23 +34,3 @@ class Soknad:
     sosken__i_barnehagen: str
     tidspunkt_oppstart: str
     brutto_inntekt: int
-    
-    
-
-
-"""
-Kontrakter:
-repr(object) funksjon (kan også bruke !r i utskriftsfunksjoner, f.eks. foresatt_id={foresatt.id!r})
-Fra [2]: "Python repr() Function returns a printable representation of an object in Python.
-In other words, we can say that the Python repr() function returns a printable
-representation of the object by converting that object to a string."
-
-"""
-
-"""
-Referanser
-[1] https://docs.python.org/3/library/dataclasses.html
-[2] https://www.geeksforgeeks.org/python-repr-function/
-
-
-"""
